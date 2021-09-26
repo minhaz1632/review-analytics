6b029ccae3a5c29f629d62fb19737b44e65d282e
minhaz1632 <minhaz1632@gmail.com>
2021-09-26 11:37:30 +0600

    Add name field to yelp business page model

--- reviews/models.py
+++ reviews/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class YelpBusinessItem(models.Model):
+    name = models.CharField(max_length=200, default='')
     url = models.URLField()
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
     date_created = models.DateTimeField(auto_now_add=True)
